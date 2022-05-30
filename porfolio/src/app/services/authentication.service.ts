09154903a04f3e6460b4e7cdd80d537563b552de
Matias <matiasquiroga.04@gmail.com>
2022-05-30 17:26:31 -0300

    Update authentication.service.ts

--- porfolio/src/app/services/authentication.service.ts
+++ porfolio/src/app/services/authentication.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { NewUser } from '../entities/new-user'
 import { LoginUser } from '../entities/login-user';
 import { JwtDto } from '../entities/jwt-dto';
-import { environment } from 'src/environments/environment';
+/*import { environment } from 'src/environments/environment';*/
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class AuthenticationService {
 
   /*cambiar this.authURL por this.URL*/
   public neww(newUser: NewUser): Observable<any> {
-    return this.httpClient.post<any>(this.authURL + 'new', newUser);
+    return this.httpClient.post<any>(this.authURL + 'neww', newUser);
   }
 
   public login(loginUser: LoginUser): Observable<JwtDto> {
