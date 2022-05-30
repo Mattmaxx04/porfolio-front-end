6fbe1fdde674e51dfaa6bee0f111865b0a6df80a
Matias <matiasquiroga.04@gmail.com>
2022-05-30 17:21:54 -0300

    Update routing

--- porfolio/src/app/app-routing.module.ts
+++ porfolio/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { PersonComponent } from './components/person/person.component';
+import { PorfolioComponent } from './components/porfolio/porfolio.component';
 
 
 const routes: Routes = [
-  { path: '', component: PersonComponent },
+  { path: '', component: PorfolioComponent },
   { path: 'login', component: LoginComponent },
 ];
 
